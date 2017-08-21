0cbec08b50adf59f424ae56944bda2bfde128cae
kruulik <karoliskrulis@gmail.com>
2017-08-21 17:45:19 -0400

    refactored App

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import GreetingContainer from './greeting_container';
 import SessionFormContainer from './session_form_container';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import {AuthRoute} from '../util/route_util.jsx';
 
 
 const App = () => (
   <div>
-    <h1>Bench BnB</h1>
-    <GreetingContainer />
+    <header>
+      <Link to="/" className="header-link">
+        <h1>Bench BnB</h1>
+      </Link>
+      <GreetingContainer />
+    </header>
 
     <AuthRoute path="/login" component={SessionFormContainer} />
     <AuthRoute path="/signup" component={SessionFormContainer} />
